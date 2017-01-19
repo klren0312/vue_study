7df8c1d864188f14e94b6bec5489df35a2e1a719
zzes <kuanglong0312@sina.com>
2017-01-19 11:48:39 +0800

    20170119

---
+++ vuetest/src/components/register.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="head">
+		<h1>{{title}}</h1>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+				title:"this is register"
+			}
+		}
+	}
+</script>
